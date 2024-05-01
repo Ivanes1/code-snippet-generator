e289c6c0eb33419815096606b3fd49c0427ae83f
Ivanes1 <ivanesmelnikov@gmail.com>
2024-05-01 20:38:01 +0900

    left panel logic in index.js

--- index.js
+++ index.js
@@ -20,6 +20,90 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const regenerateCodeBtn = document.getElementById("regenerateCodeBtn");
 
+  function loadSnippets() {
+    snippetsList.innerHTML = null;
+    const snippets = JSON.parse(localStorage.getItem("snippets")) || [];
+    snippets.forEach((snippet) => {
+      const li = document.createElement("li");
+      li.classList.add("flex", "justify-between", "mb-4");
+
+      const activateSnippetBtn = document.createElement("a");
+      activateSnippetBtn.classList.add(
+        "w-full",
+        "block",
+        "p-2",
+        "bg-gray-300",
+        "rounded"
+      );
+      activateSnippetBtn.href = "#";
+      activateSnippetBtn.textContent = snippet.name;
+      activateSnippetBtn.addEventListener("click", () => {
+        activateSnippet(snippet.id);
+      });
+
+      const deleteSnippetBtn = document.createElement("button");
+      deleteSnippetBtn.classList.add(
+        "bg-red-500",
+        "text-white",
+        "px-2",
+        "py-1",
+        "rounded"
+      );
+      deleteSnippetBtn.textContent = "Delete";
+      deleteSnippetBtn.addEventListener("click", () => {
+        deleteSnippet(snippet.id);
+      });
+
+      const activeSnippetIndicator = document.createElement("a");
+      activeSnippetIndicator.classList.add(
+        "w-full",
+        "block",
+        "p-2",
+        "bg-gray-500",
+        "rounded",
+        "text-white"
+      );
+      activeSnippetIndicator.href = "#";
+      activeSnippetIndicator.textContent = snippet.name;
+
+      li.appendChild(activateSnippetBtn);
+      li.appendChild(deleteSnippetBtn);
+      li.appendChild(activeSnippetIndicator);
+      snippetsList.appendChild(li);
+    });
+  }
+
+  function activateSnippet(id) {
+    // get call to fetch the snippet data from the server
+  }
+
+  function deleteSnippet(id) {
+    const snippets = JSON.parse(localStorage.getItem("snippets")) || [];
+    const updatedSnippets = snippets.filter((s) => s.id !== id);
+    localStorage.setItem("snippets", JSON.stringify(updatedSnippets));
+
+    loadSnippets();
+  }
+
+  function createSnippet(name) {
+    const id = "snippet-" + Date.now();
+    const snippets = JSON.parse(localStorage.getItem("snippets")) || [];
+    snippets.push({ id, name });
+    localStorage.setItem("snippets", JSON.stringify(snippets));
+
+    codeBlock.textContent = "";
+    testsBlock.textContent = "";
+
+    loadSnippets();
+  }
+
+  createSnippetBtn.addEventListener("click", () => {
+    const snippetName = prompt("Enter the name of the new snippet:");
+    if (snippetName) {
+      createSnippet(snippetName);
+    }
+  });
+
   function generateCode(prompt) {
     const payload = {
       prompt,
