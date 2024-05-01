a7899119d717471516f418aa885c318dd8ff4962
Ivanes1 <ivanesmelnikov@gmail.com>
2024-05-01 20:58:20 +0900

    tester for the other languages

--- code_tester.py
+++ code_tester.py
@@ -14,14 +14,148 @@ class CodeTester:
         self.language = langs[0] if langs else "python"
         self.language_tester = {
             "python": self._test_python,
-            # "javascript": self._test_javascript,
-            # "java": self._test_java,
-            # "ruby": self._test_ruby,
-            # "c++": self._test_cpp,
+            "javascript": self._test_javascript,
+            "java": self._test_java,
+            "ruby": self._test_ruby,
+            "c++": self._test_cpp,
         }
 
     def test(self):
         return self.language_tester[self.language]()
+
+    def _test_cpp(self):
+        test_lines = [test + ";" for test in self.tests if not test.endswith(";")]
+        script = "\n".join(
+            ["#include <assert.h>\n", self.code, "\nint main() {", *test_lines, "}"]
+        )
+        print(script)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".exe") as temp_exe:
+            exe_name = temp_exe.name
+
+        try:
+            compile_process = subprocess.run(
+                ("g++", "-xc++", "-", "-o", exe_name),
+                input=script,
+                encoding=locale.getpreferredencoding(),
+                capture_output=True,
+            )
+
+            if compile_process.returncode != 0:
+                return {
+                    "error": "Compilation failed",
+                    "message": compile_process.stderr,
+                }
+
+            run_process = subprocess.run((exe_name), capture_output=True, text=True)
+            if run_process.returncode != 0:
+                return {"error": "Runtime failed", "message": run_process.stderr}
+
+            return {"message": "All tests passed!"}
+
+        except Exception as e:
+            return {"error": "Code execution failed", "message": str(e)}
+
+        finally:
+            os.remove(exe_name)
+
+    def _test_ruby(self):
+        script = "\n".join([self.code, "\n"] + self.tests)
+        print(script)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".rb") as temp_rb:
+            temp_rb.write(script.encode())
+            script_path = temp_rb.name
+
+        try:
+            run_process = subprocess.run(
+                ("ruby", script_path),
+                capture_output=True,
+                text=True,
+            )
+            if run_process.returncode != 0:
+                return {
+                    "error": "Code execution failed",
+                    "message": run_process.stderr,
+                }
+            return {"message": "All tests passed!"}
+        except Exception as e:
+            return {"error": "Code execution failed", "message": str(e)}
+        finally:
+            os.remove(script_path)
+
+    def _test_java(self):
+        class_code = "\n".join(
+            [
+                "public class TestRunner {",
+                "public static void main(String[] args) {",
+                "try {",
+                *self.tests,
+                "} catch (Exception e) {",
+                "System.out.println(e.toString());",
+                "System.exit(1);",
+                "}",
+                "System.exit(0);",
+                "}",
+                self.code,
+                "}",
+            ]
+        )
+        print(class_code)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".java") as temp_java:
+            temp_java.write(class_code.encode())
+            class_path = temp_java.name
+
+        try:
+            compile_process = subprocess.run(
+                ("javac", class_path),
+                capture_output=True,
+                text=True,
+            )
+            if compile_process.returncode != 0:
+                return {
+                    "error": "Compilation failed",
+                    "message": compile_process.stderr,
+                }
+
+            run_process = subprocess.run(
+                ("java", "-cp", os.path.dirname(class_path), "TestRunner"),
+                capture_output=True,
+                text=True,
+            )
+            if run_process.returncode != 0:
+                return {
+                    "error": "Runtime failed",
+                    "message": run_process.stderr,
+                }
+            return {"message": "All tests passed!"}
+        except Exception as e:
+            return {"error": "Code execution failed", "message": str(e)}
+        finally:
+            os.remove(class_path)
+            os.remove(class_path.replace(".java", ".class"))
+
+    def _test_javascript(self):
+        script = "\n".join([self.code, "\n"] + self.tests)
+        print(script)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".js") as temp_js:
+            temp_js.write(script.encode())
+            script_path = temp_js.name
+
+        try:
+            run_process = subprocess.run(
+                ("node", script_path),
+                capture_output=True,
+                text=True,
+            )
+            if run_process.returncode != 0:
+                return {
+                    "error": "Code execution failed",
+                    "message": run_process.stderr,
+                }
+            return {"message": "All tests passed!"}
+        except Exception as e:
+            return {"error": "Code execution failed", "message": str(e)}
+        finally:
+            os.remove(script_path)
 
     def _test_python(self):
         script = "\n".join([self.code, "\n", *self.tests])
